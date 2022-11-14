113c804f54929e9086a66d39dd6729b5c667e980
Samson Njuguna <96062644+samnjuguna44@users.noreply.github.com>
2022-11-14 12:12:40 +0300

    Added content to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,54 +33,6 @@ const Main = () => {
         quo et aperiam quibusdam provident quia corrupti animi ullam, ea
         ratione?
       </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, ab
-        porro temporibus neque omnis dignissimos eveniet cupiditate iure nemo
-        quo et aperiam quibusdam provident quia corrupti animi ullam, ea
-        ratione?
-      </p>
     </div>
   );
 };
